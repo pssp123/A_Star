9b188427efb9f2135a8e9bd060746fe6687e4491
pssp123 <380870978@qq.com>
2018-06-04 13:37:49 +0800

    fix():Fix didnt init the variable of map

---
+++ include/PathAlgorithm.hpp
@@ -0,0 +1,16 @@
+//
+// Created by pierre on 18-6-4.
+//
+
+#ifndef A_START_PATHALGORITHM_HPP
+#define A_START_PATHALGORITHM_HPP
+
+
+
+class PathAlgorithm {
+
+};
+
+
+
+#endif //A_START_PATHALGORITHM_HPP
---
+++ src/PathAlgorithm.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pierre on 18-6-4.
+//
+
+#include "PathAlgorithm.hpp"
